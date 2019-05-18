94fb38d15e4e92df2c04a83399374a2d88eed9b9
iMac <imac@192-168-0-104.rdsnet.ro>
2019-05-18 10:58:04 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by iMac on 18.05.2019.
+//  Copyright © 2019 Cornel Siclovan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
