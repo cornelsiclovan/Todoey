b3189a8e550d7b847027ff2629118a6f94e97b70
iMac <imac@192-168-0-104.rdsnet.ro>
2019-05-18 19:55:54 +0300

    Convert to mvc design pattern

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by iMac on 18.05.2019.
+//  Copyright © 2019 Cornel Siclovan. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
